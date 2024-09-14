9645d7e3ee7bbdafd45bf217f21101c24aa6fe28
walid cadi <cadiwalid02@gmail.com>
2024-09-14 05:15:01 +0100

    login

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { MyAppContext } from "../../context";
 
 export const Auth = () => {
   const navigate = useNavigate();
+  const { userData, setUserData } = MyAppContext();
+  const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  const [loginMessage, setLoginMessage] = useState(""); 
+    
+    //console.log(userData);
+    
+    const handleLogin =  (e) => {
+    e.preventDefault();
+    const user = userData.find(
+      (profile) => profile.email === email && profile.password === password
+    );
+    if (user) {
+        setLoginMessage("Login successful!");
+        setTimeout(() => {
+            navigate("/")
+        }, 3000);
+    } else {
+      setLoginMessage("Invalid email or password. Please try again.");
+    }
+  };
+
   return (
     <div className="mt-[14.5vh] p-20 flex gap-x-8">
       <div className="p-7 border-gray-400 border-2 w-[40vw]">
@@ -26,36 +49,51 @@ export const Auth = () => {
         <p className="text-lg font-bold text-gray-500">
           I am a returning customer
         </p>
-        <form action="" className="flex flex-col gap-y-3">
+        {loginMessage && (
+          <p
+            className={`text-center mb-4 ${
+              loginMessage.includes("successful")
+                ? "text-green-500"
+                : "text-red-500"
+            }`}>
+            {loginMessage}
+          </p>
+        )}
+        <form
+          action=""
+          onSubmit={handleLogin}
+          className="flex flex-col gap-y-3">
           <label htmlFor="">Email</label>
           <input
             className="border-gray-400 border-2 px-3 py-2"
             type="email"
             placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor="">Password</label>
           <input
             className="border-gray-400 border-2 px-3 py-2"
-            type="email"
+            type="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
+          <div className="flex items-center gap-1">
+            <button className="px-8 py-3 bg-[#292929] text-white mt-3">
+              SIGN IN
+            </button>
+            <p>
+              {" "}
+              or{" "}
+              <span
+                onClick={() => {
+                  navigate("/");
+                }}
+                className="cursor-pointer hover:text-[#e65540]">
+                Return to Store
+              </span>
+            </p>
+          </div>
         </form>
-        <div className="flex items-center gap-1">
-          <button className="px-8 py-3 bg-[#292929] text-white mt-3">
-            SIGN IN
-          </button>
-          <p>
-            {" "}
-            or{" "}
-            <span
-              onClick={() => {
-                navigate("/");
-              }}
-              className="cursor-pointer hover:text-[#e65540]">
-              Return to Store
-            </span>
-          </p>
-        </div>
       </div>
     </div>
   );
